9adb97efc89fc53c4b4f18cd2638b303870cb80f
yukiti2007 <yukiti2019@gmail.com>
2019-08-08 17:13:10 +0800

    flask router

---
+++ python/flask/router.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/hello1", methods=["GET", "POST"])
+def hello_world1():
+    return "hello_world1"
+
+
+def hello_world2():
+    return "hello world2"
+
+
+app.add_url_rule("/hello2", view_func=hello_world2, methods=["GET", "POST"])
+
+app.run(host="0.0.0.0", port=8080, debug=True)
