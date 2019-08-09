a28375ad5dd04fad73b51bb112b9412fc8a144be
yukiti2007 <yukiti2019@gmail.com>
2019-08-09 10:59:47 +0800

    twisted

---
+++ python/twisted/client.py
@@ -0,0 +1,84 @@
+import datetime
+import time
+
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol, ClientFactory
+
+
+class TcpClient(Protocol):
+    SERVER_MAP = {}
+
+    def __init__(self):
+        self.connected = False
+
+    def connectionMade(self):
+        self.connected = True
+        addr = self.transport.addr
+        print("connected", self.transport.socket)
+        client_ip = addr[0]
+        TcpClient.SERVER_MAP[client_ip] = self
+        nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        str = "你好服务器，我是客户端 " + nowTime
+        self.transport.write(str.encode("utf-8"))
+
+    def connectionLost(self, reason):
+        self.connected = False
+        addr = self.transport.addr
+        client_ip = addr[0]
+        if client_ip in TcpClient.SERVER_MAP:
+            del TcpClient.SERVER_MAP[client_ip]
+
+    def dataReceived(self, tcp_data):
+        addr = self.transport.addr
+        nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        try:
+            msg = tcp_data.decode("utf-8")
+            print("Received msg", msg, "from Tcp Server", addr)
+
+            time.sleep(5)
+            str = "来自客户端的响应 " + nowTime
+            self.transport.write(str.encode("utf-8"))
+
+        except BaseException as e:
+            print("Comd Execute Error from", addr, "data:", tcp_data)
+            str = "客户端发生异常 " + nowTime
+            self.transport.write(str.encode("utf-8"))
+
+
+class TcpClientFactory(ClientFactory):
+    def __init__(self):
+        self.protocol = None
+
+    def startedConnecting(self, connector):
+        print("Starting Connecting To Tcp Server", (connector.host, connector.port))
+
+    def buildProtocol(self, addr):
+        print("Connected To Tcp Server", addr)
+        self.protocol = TcpClient()
+        return self.protocol
+
+    def clientConnectionLost(self, connector, reason):
+        print("Lost Connection from Tcp Server", (connector.host, connector.port), 'Reason:', reason)
+        time.sleep(30)
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print("Failed To Connect To Tcp Server", (connector.host, connector.port), 'Reason:', reason)
+        time.sleep(30)
+        connector.connect()
+
+
+# 连接到服务器
+def start_tcp_client():
+    hosts = ["127.0.0.1"]
+    port = 9527
+    factory = TcpClientFactory()
+
+    for host in hosts:
+        reactor.connectTCP(host, port, factory)
+
+    reactor.run()
+
+
+if __name__ == "__main__":
+    start_tcp_client()
---
+++ python/twisted/server.py
@@ -0,0 +1,68 @@
+# coding=utf-8
+import datetime
+import time
+
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.protocol import Factory
+from twisted.internet.protocol import Protocol
+
+
+class TcpServer(Protocol):
+    CLIENT_MAP = {}
+
+    def __init__(self, factory):
+        self.connected = False
+        self.factory = factory
+
+    def connectionMade(self):
+        self.connected = True
+        self.factory.numProtocols = self.factory.numProtocols + 1
+        addr = self.transport.client
+        print("connected", self.transport.socket)
+        TcpServer.CLIENT_MAP[addr] = self
+
+    def connectionLost(self, reason):
+        self.connected = False
+        self.factory.numProtocols = self.factory.numProtocols - 1
+        addr = self.transport.client
+        if addr in TcpServer.CLIENT_MAP:
+            print(addr, "Lost Connection from Tcp Server", 'Reason:', reason)
+            del TcpServer.CLIENT_MAP[addr]
+
+    def dataReceived(self, tcp_data):
+        addr = self.transport.client
+        nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        try:
+            msg = tcp_data.decode("utf-8")
+            print("Received msg", msg, "from Tcp Client", addr)
+
+            time.sleep(5)
+            str = "来自服务器的响应 " + nowTime
+            self.transport.write(str.encode("utf-8"))
+
+        except BaseException as e:
+            print("Comd Execute Error from", addr, "data:", tcp_data)
+            str = "服务器发生异常 " + nowTime
+            self.transport.write(str.encode("utf-8"))
+
+
+class TcpServerFactory(Factory):
+    def __init__(self):
+        self.numProtocols = 0
+
+    def buildProtocol(self, addr):
+        return TcpServer(self)
+
+
+# 启动tcp服务端
+def start_tcp_server():
+    port = 9527
+    endpoint = TCP4ServerEndpoint(reactor, port)
+    endpoint.listen(TcpServerFactory())
+    print("#####", "Starting TCP Server on", port, "#####")
+    reactor.run()
+
+
+if __name__ == "__main__":
+    start_tcp_server()
