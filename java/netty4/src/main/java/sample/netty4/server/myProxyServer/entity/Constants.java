8f7bf8eb4a62bb9f4bddff56b1471016e727d9db
yukiti2007 <yukiti2019@gmail.com>
2019-08-21 18:27:38 +0800

    netty MyTestProxy

--- java/netty4/src/main/java/sample/netty4/server/myProxyServer/entity/Constants.java
+++ java/netty4/src/main/java/sample/netty4/server/myProxyServer/entity/Constants.java
@@ -7,20 +7,13 @@ public class Constants {
 
     public static final int CONNECT_TIMOUT_MS = 20000;
 
-    public static final ImmutableSet<String> DROP_HEADERS =
-            ImmutableSet.of(
-                    HttpHeaders.PROXY_AUTHORIZATION,
-                    HttpHeaders.X_FORWARDED_FOR);
+    public static final ImmutableSet<String> DROP_HEADERS = ImmutableSet.of(
+            HttpHeaders.PROXY_AUTHORIZATION,
+            HttpHeaders.X_FORWARDED_FOR);
 
-    public static final ImmutableSet HTTP_METHOD = (new ImmutableSet.Builder())
-            .add("HEAD")
-            .add("GET")
-            .add("POST")
-            .add("PUT")
-            .add("DELETE")
-            .build();
+    public static final ImmutableSet HTTP_METHOD = ImmutableSet.of(
+            "HEAD", "GET", "POST", "PUT", "DELETE");
 
-    public static final ImmutableSet HTTPS_METHOD = (new ImmutableSet.Builder())
-            .add("CONNECT")
-            .build();
+    public static final ImmutableSet HTTPS_METHOD = ImmutableSet.of(
+            "CONNECT");
 }
