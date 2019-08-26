cc272cb88b79a3d062a7cda8f83510f97efc9bc0
yukiti2007 <yukiti2019@gmail.com>
2019-08-26 17:30:13 +0800

    netty MyTestProxy

--- java/netty4/src/main/java/sample/netty4/server/myProxyServer/entity/EventLoopGroups.java
+++ java/netty4/src/main/java/sample/netty4/server/myProxyServer/entity/EventLoopGroups.java
@@ -4,6 +4,6 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 
 public class EventLoopGroups {
-    public static final EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-    public static final EventLoopGroup workerGroup = new NioEventLoopGroup(16);
+    public static final EventLoopGroup bossGroup = new NioEventLoopGroup();
+    public static final EventLoopGroup workerGroup = new NioEventLoopGroup();
 }
