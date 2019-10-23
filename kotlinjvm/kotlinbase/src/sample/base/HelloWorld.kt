d309e6a9a7e406378b6547dafaba5dac6ee24e02
yukiti2007 <yukiti2019@gmail.com>
2019-10-23 15:21:44 +0800

    kotlin base

--- kotlinjvm/kotlinbase/src/sample/base/HelloWorld.kt
+++ kotlinjvm/kotlinbase/src/sample/base/HelloWorld.kt
@@ -2,9 +2,15 @@ package sample.base
 
 class Hello constructor() {
     var obj: String = "World"
+    var sign: String = ""
+        set
 
     fun sayHello() {
-        println("Hello$obj")
+        if ("".equals(sign)) {
+            println("Hello $obj by $sign")
+        } else {
+            println("Hello $obj")
+        }
     }
 
     constructor(obj: String) : this() {
@@ -16,7 +22,11 @@ class Hello constructor() {
 fun main(args: Array<String>) {
     val helloWorld = Hello();
     helloWorld.sayHello()
+    helloWorld.sign = "yukiti"
+    helloWorld.sayHello()
 
     val helloKotlin = Hello("Kotlin")
+    helloKotlin.sayHello()
+    helloKotlin.sign = "yukiti"
     helloKotlin.sayHello()
 }
